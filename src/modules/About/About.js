ea266ac1a235afcfb1f8549324801e8a888044c8
VadimGalian <vadim.galian@gmail.com>
2023-02-16 17:18:58 +0200

    intermediate

--- src/modules/About/About.js
+++ src/modules/About/About.js
@@ -1,10 +1,11 @@
 import { Section, SectionTitle } from "modules/common/ui"
 import { AboutList } from "./components/AboutList/AboutList"
+import styles from "./About.module.css"
 
 export function About() {
     return (
         <Section>
-            <SectionTitle>
+            <SectionTitle className={styles.title}>
                 I Know that <span className="accent">Good Design</span> <br />
                 means <span className="accent">Good Business</span>
             </SectionTitle>
