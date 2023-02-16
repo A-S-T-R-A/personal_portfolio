3881455e34810854f35688ad75aa5692a554f94f
VadimGalian <vadim.galian@gmail.com>
2023-02-16 15:42:40 +0200

    add logo

--- src/modules/Navigation/Navigation.js
+++ src/modules/Navigation/Navigation.js
@@ -3,11 +3,15 @@ import styles from "./Navigation.module.css"
 
 import { Burger, Navbar } from "./components"
 
+import Art1us from "../../assets/images/common/art1us.png"
+
 function Navigation() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
-                <div className={styles.logoContainer}>Portfolio</div>
+                <div className={styles.logoCont}>
+                    <img src={Art1us} className={styles.logo} />
+                </div>
                 <Navbar />
                 <Burger />
             </div>
