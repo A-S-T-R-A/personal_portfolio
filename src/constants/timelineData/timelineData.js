9c273e740e46f8e27d941327c272f2774eae4249
VadimGalian <vadim.galian@gmail.com>
2023-02-17 15:28:51 +0200

    intermediate

--- src/constants/timelineData/timelineData.js
+++ src/constants/timelineData/timelineData.js
@@ -1,32 +1,10 @@
 export const timelineData = [
     {
-        id: 1,
-        year: 2017,
-        works: [
-            {
-                name: "Google",
-                company: "Google",
-                description: "Worked as a freelance developer",
-            },
-        ],
-    },
-    {
-        id: 2,
-        year: 2018,
-        works: [
-            {
-                name: "Google",
-                company: "Google",
-                description: "Worked as a freelance developer",
-            },
-        ],
-    },
-    {
         id: 3,
-        year: 2019,
+        year: 2020,
         works: [
             {
-                name: "Google",
+                name: "Front End Developer",
                 company: "Google",
                 description: "Worked as a freelance developer",
             },
@@ -34,22 +12,22 @@ export const timelineData = [
     },
     {
         id: 4,
-        year: 2020,
+        year: 2021,
         works: [
             {
-                name: "Google",
-                company: "Google",
+                name: "Blockchain",
+                company: "Meta",
                 description: "Worked as a freelance developer",
             },
         ],
     },
     {
         id: 5,
-        year: 2021,
+        year: 2022,
         works: [
             {
-                name: "Google",
-                company: "Google",
+                name: "Back End Developer",
+                company: "Meta",
                 description: "Worked as a freelance developer",
             },
         ],
