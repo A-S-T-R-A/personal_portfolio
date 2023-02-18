c66c0ff579de484cc0a8c1c8cedf897d9769ec14
je-delray <hovax74935@ekcsoft.com>
2023-02-18 13:49:41 +0100

    preview changes

--- src/assets/images/projects/index.js
+++ src/assets/images/projects/index.js
@@ -1,6 +1,25 @@
-import projImg1 from "./1.png"
-import projImg2 from "./2.png"
-import projImg3 from "./3.jpg"
-import projImg4 from "./4.jpg"
+import projImg1_1 from "./1.1.jpg";
+import projImg1_2 from "./1.2.jpg";
+import projImg1_3 from "./1.3.jpg";
 
-export { projImg1, projImg2, projImg3, projImg4 }
+import projImg2_1 from "./2.1.jpg";
+import projImg2_2 from "./2.2.jpg";
+import projImg2_3 from "./2.3.jpg";
+
+import projImg3_1 from "./3.1.jpg";
+import projImg3_2 from "./3.2.jpg";
+import projImg3_3 from "./3.3.jpg";
+
+import projImg4_1 from "./4.1.jpg";
+import projImg4_2 from "./4.2.jpg";
+import projImg4_3 from "./4.3.jpg";
+
+import projImg5_1 from "./5.1.jpg";
+import projImg5_2 from "./5.2.jpg";
+import projImg5_3 from "./5.3.jpg";
+
+export { projImg1_1, projImg1_2, projImg1_3 }; // beats
+export { projImg2_1, projImg2_2, projImg2_3 }; // delivery
+export { projImg3_1, projImg3_2, projImg3_3 }; // halloween
+export { projImg4_1, projImg4_2, projImg4_3 }; // sushi
+export { projImg5_1, projImg5_2, projImg5_3 }; // flight agency
