7d615ec682f5e2ec03b92185213ce197a26ff2d9
Art1us <109896229+Art1us@users.noreply.github.com>
2023-02-18 14:21:12 +0200

    upd footer

--- src/modules/Footer/Footer.js
+++ src/modules/Footer/Footer.js
@@ -1,28 +1,29 @@
 import { SocialIcons } from "modules/common/components/SocialIcons"
-import React from "react"
-
 import styles from "./Footer.module.css"
 
-const Footer = () => {
+function Footer() {
     return (
         <footer className={styles.wrapper} id="footer">
             <div className={styles.container}>
                 <div className={styles.content}>
                     <div className={styles.phone}>
-                        <h4 className={styles.linkTitle}>Call</h4>
-                        <a className={styles.link} href="tel:314-343-3432">
-                            314-343-3432
+                        <h4 className={styles.linkTitle}>Skype</h4>
+                        <a
+                            className={styles.link}
+                            href="skype:live:artur0503915024"
+                        >
+                            Artur Pazyniuk
                         </a>
                     </div>
                     <div className={styles.email}>
                         <h4 className={styles.linkTitle}>Email</h4>
-                        <a className={styles.link} href="Artius@shmartius.com">
-                            Artius@shmartius.com
+                        <a
+                            className={styles.link}
+                            href="mailto:PazyniukArtur@gmail.com"
+                        >
+                            PazyniukArtur@gmail.com
                         </a>
                     </div>
-                    {/* <p className={styles.slogan}>
-                        Innovating one project at a time
-                    </p> */}
                     <div className={styles.socialIcons}>
                         <SocialIcons className={styles.icons} />
                     </div>
