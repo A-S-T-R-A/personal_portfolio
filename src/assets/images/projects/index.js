3a630fe01a40e47028ea1e3a5916311b9e774307
je-delray <hovax74935@ekcsoft.com>
2023-02-21 22:04:48 +0100

    66

--- src/assets/images/projects/index.js
+++ src/assets/images/projects/index.js
@@ -18,8 +18,23 @@ import projImg5_1 from "./5.1.jpg";
 import projImg5_2 from "./5.2.jpg";
 import projImg5_3 from "./5.3.jpg";
 
+import projImg6_1 from "./6.1.jpg";
+import projImg6_2 from "./6.2.jpg";
+import projImg6_3 from "./6.3.jpg";
+
+import projImg7_1 from "./7.1.jpg";
+import projImg7_2 from "./7.2.jpg";
+import projImg7_3 from "./7.3.jpg";
+
+import projImg8_1 from "./8.1.jpg";
+import projImg8_2 from "./8.2.jpg";
+import projImg8_3 from "./8.3.jpg";
+
 export { projImg1_1, projImg1_2, projImg1_3 }; // beats
 export { projImg2_1, projImg2_2, projImg2_3 }; // delivery
 export { projImg3_1, projImg3_2, projImg3_3 }; // halloween
 export { projImg4_1, projImg4_2, projImg4_3 }; // sushi
 export { projImg5_1, projImg5_2, projImg5_3 }; // flight agency
+export { projImg6_1, projImg6_2, projImg6_3 }; // restaurant React
+export { projImg7_1, projImg7_2, projImg7_3 }; // watches
+export { projImg8_1, projImg8_2, projImg8_3 }; // christmas
