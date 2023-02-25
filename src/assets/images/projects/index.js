177aaa51e49fd28a60b2423abb2fbe437f3bc1cd
je-delray <hovax74935@ekcsoft.com>
2023-02-25 18:24:32 +0100

    third commit

--- src/assets/images/projects/index.js
+++ src/assets/images/projects/index.js
@@ -1,40 +1,50 @@
-import projImg1_1 from "./1.1.jpg";
-import projImg1_2 from "./1.2.jpg";
-import projImg1_3 from "./1.3.jpg";
+import projImg1_1 from "./1.1.jpg"
+import projImg1_2 from "./1.2.jpg"
+import projImg1_3 from "./1.3.jpg"
 
-import projImg2_1 from "./2.1.jpg";
-import projImg2_2 from "./2.2.jpg";
-import projImg2_3 from "./2.3.jpg";
+import projImg2_1 from "./2.1.jpg"
+import projImg2_2 from "./2.2.jpg"
+import projImg2_3 from "./2.3.jpg"
 
-import projImg3_1 from "./3.1.jpg";
-import projImg3_2 from "./3.2.jpg";
-import projImg3_3 from "./3.3.jpg";
+import projImg3_1 from "./3.1.jpg"
+import projImg3_2 from "./3.2.jpg"
+import projImg3_3 from "./3.3.jpg"
 
-import projImg4_1 from "./4.1.jpg";
-import projImg4_2 from "./4.2.jpg";
-import projImg4_3 from "./4.3.jpg";
+import projImg4_1 from "./4.1.jpg"
+import projImg4_2 from "./4.2.jpg"
+import projImg4_3 from "./4.3.jpg"
 
-import projImg5_1 from "./5.1.jpg";
-import projImg5_2 from "./5.2.jpg";
-import projImg5_3 from "./5.3.jpg";
+import projImg5_1 from "./5.1.jpg"
+import projImg5_2 from "./5.2.jpg"
+import projImg5_3 from "./5.3.jpg"
 
-import projImg6_1 from "./6.1.jpg";
-import projImg6_2 from "./6.2.jpg";
-import projImg6_3 from "./6.3.jpg";
+import projImg6_1 from "./6.1.jpg"
+import projImg6_2 from "./6.2.jpg"
+import projImg6_3 from "./6.3.jpg"
 
-import projImg7_1 from "./7.1.jpg";
-import projImg7_2 from "./7.2.jpg";
-import projImg7_3 from "./7.3.jpg";
+import projImg7_1 from "./7.1.jpg"
+import projImg7_2 from "./7.2.jpg"
+import projImg7_3 from "./7.3.jpg"
 
-import projImg8_1 from "./8.1.jpg";
-import projImg8_2 from "./8.2.jpg";
-import projImg8_3 from "./8.3.jpg";
+import projImg8_1 from "./8.1.jpg"
+import projImg8_2 from "./8.2.jpg"
+import projImg8_3 from "./8.3.jpg"
 
-export { projImg1_1, projImg1_2, projImg1_3 }; // beats
-export { projImg2_1, projImg2_2, projImg2_3 }; // delivery
-export { projImg3_1, projImg3_2, projImg3_3 }; // halloween
-export { projImg4_1, projImg4_2, projImg4_3 }; // sushi
-export { projImg5_1, projImg5_2, projImg5_3 }; // flight agency
-export { projImg6_1, projImg6_2, projImg6_3 }; // restaurant React
-export { projImg7_1, projImg7_2, projImg7_3 }; // watches
-export { projImg8_1, projImg8_2, projImg8_3 }; // christmas
+import projImg9_1 from "./9.1.jpg"
+import projImg9_2 from "./9.2.jpg"
+import projImg9_3 from "./9.3.jpg"
+
+import projImg10_1 from "./10.1.jpg"
+import projImg10_2 from "./10.2.jpg"
+import projImg10_3 from "./10.3.jpg"
+
+export { projImg1_1, projImg1_2, projImg1_3 } // beats
+export { projImg2_1, projImg2_2, projImg2_3 } // delivery
+export { projImg3_1, projImg3_2, projImg3_3 } // halloween
+export { projImg4_1, projImg4_2, projImg4_3 } // sushi
+export { projImg5_1, projImg5_2, projImg5_3 } // flight agency
+export { projImg6_1, projImg6_2, projImg6_3 } // restaurant React
+export { projImg7_1, projImg7_2, projImg7_3 } // watches
+export { projImg8_1, projImg8_2, projImg8_3 } // christmas
+export { projImg9_1, projImg9_2, projImg9_3 } // travel
+export { projImg10_1, projImg10_2, projImg10_3 } // small restaurant
