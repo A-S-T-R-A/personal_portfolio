9367889be202c7f2a107164eb2b38057c6a30a82
je-delray <hovax74935@ekcsoft.com>
2023-02-28 12:00:35 +0100

    4th

--- src/assets/images/projects/index.js
+++ src/assets/images/projects/index.js
@@ -18,11 +18,12 @@ import projImg5_1 from "./5.1.jpg"
 import projImg5_2 from "./5.2.jpg"
 import projImg5_3 from "./5.3.jpg"
 
-import projImg9_1 from "./9.1.jpg"
-import projImg10_1 from "./10.1.jpg"
+import projImg9_1 from "./9.1.png"
+import projImg10_1 from "./10.1.png"
 
 export { projImg1_1, projImg1_2, projImg1_3 } // beats
 export { projImg2_1, projImg2_2, projImg2_3 } // delivery
 export { projImg3_1, projImg3_2, projImg3_3 } // halloween
 export { projImg4_1, projImg4_2, projImg4_3 } // sushi
 export { projImg5_1, projImg5_2, projImg5_3 } // flight agency
+export { projImg9_1, projImg10_1 }
