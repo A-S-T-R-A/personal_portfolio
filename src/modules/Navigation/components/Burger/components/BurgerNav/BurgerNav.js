79def751bf260c255c293cb9336012b7335a2c4b
VadimGalian <vadim.galian@gmail.com>
2023-03-02 14:44:40 +0200

    test height 1

--- src/modules/Navigation/components/Burger/components/BurgerNav/BurgerNav.js
+++ src/modules/Navigation/components/Burger/components/BurgerNav/BurgerNav.js
@@ -1,9 +1,11 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { BurgerCta } from "../"
 import { NavigationList } from "../../../common"
 import styles from "./BurgerNav.module.css"
 
 export function BurgerNav({ showBurger, setShowBurger }) {
+    const [height, setHeight] = useState(window.innerHeight)
+
     function menuClickHandler(e) {
         e.stopPropagation()
     }
@@ -18,6 +20,7 @@ export function BurgerNav({ showBurger, setShowBurger }) {
         <div
             className={`${styles.overlay} ${showBurger ? styles.navOpen : ""}`}
             onClick={closeClickHandler}
+            style={{ height }}
         >
             <div className={styles.wrapper} onClick={menuClickHandler}>
                 <div className={styles.container}>
