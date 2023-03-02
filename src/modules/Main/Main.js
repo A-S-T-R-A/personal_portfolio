8707592b04419e3af28282abc2e421ec5a8d855f
VadimGalian <vadim.galian@gmail.com>
2023-03-02 18:15:35 +0200

    intermediate 2

--- src/modules/Main/Main.js
+++ src/modules/Main/Main.js
@@ -1,11 +1,28 @@
+import { useState, useEffect } from "react"
 import styles from "./Main.module.css"
 import { Hero } from "./components/Hero"
 import { PlanetAnimation } from "./components/PlanetAnimation"
 
 function Main() {
+    const [height, setHeight] = useState(window.innerHeight)
+
+    useEffect(() => {
+        function handleResize() {
+            setHeight(window.innerHeight)
+        }
+
+        window.addEventListener("resize", handleResize)
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [])
+
     return (
         <main className={styles.wrapper} id="home">
-            <section className={styles.container}>
+            <section
+                className={styles.container}
+                style={{ height: `${height}px` }}
+            >
                 <Hero />
                 <PlanetAnimation />
             </section>
