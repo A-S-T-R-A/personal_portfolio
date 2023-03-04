8bd3490808caf037f14393a92e5d4c62f442bbbd
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-04 10:25:14 +0200

    upd social icons links

--- src/constants/socialsData/index.js
+++ src/constants/socialsData/index.js
@@ -3,12 +3,12 @@ import { AiFillGithub, AiFillFacebook, AiFillLinkedin } from "react-icons/ai"
 export const socialsData = [
     {
         id: 1,
-        link: "https://www.facebook.com/profile.php?id=100085196048339",
+        link: "https://www.facebook.com/profile.php?id=100090508822042",
         icon: <AiFillFacebook />,
     },
     {
         id: 2,
-        link: "https://www.linkedin.com/in/artur-pazyniuk-9a2bbb266/",
+        link: "https://www.linkedin.com/in/artur-pazyniuk/",
         icon: <AiFillLinkedin />,
     },
     { id: 3, link: "https://github.com/Art1us", icon: <AiFillGithub /> },
