6391ca2e1292246faaead523e2be54ddf6045e0b
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-04 12:35:56 +0200

    resolve merge conflict

--- src/constants/projectsData/index.js
+++ src/constants/projectsData/index.js
@@ -12,7 +12,23 @@ export const projectsData = [
             "Find your dream home with our extensive collection of real estate listings.",
         img: realtorImg,
         imgDesktop: realtorImg,
-        technologies: ["HTML", "JavaScript", "CSS"],
+        technologies: [
+            {
+                title: "HTML",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "JavaScript",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "CSS",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+        ],
         visit: "https://real-estate-sable-six.vercel.app/",
         id: 8,
     },
@@ -22,7 +38,23 @@ export const projectsData = [
             "Immerse yourself in high-quality sound with our premium noise-cancelling headphones, designed for comfort and performance.",
         img: beats2Img,
         imgDesktop: beats2Img,
-        technologies: ["HTML", "JavaScript", "CSS"],
+        technologies: [
+            {
+                title: "HTML",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "JavaScript",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "CSS",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+        ],
         visit: "https://ecommerce-art1us.vercel.app/",
         id: 3,
     },
@@ -32,7 +64,23 @@ export const projectsData = [
             "Book your next adventure with our easy-to-use flight booking app, designed to help you find the best deals and plan your travel itinerary with ease.",
         img: planeImg,
         imgDesktop: planeImg,
-        technologies: ["HTML", "JavaScript", "CSS"],
+        technologies: [
+            {
+                title: "HTML",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "JavaScript",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "CSS",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+        ],
         visit: "https://unrivaled-douhua-a237e4.netlify.app/",
         id: 12,
     },
@@ -42,7 +90,23 @@ export const projectsData = [
             "Experience an unforgettable dining journey at our restaurant, where we use the freshest and highest quality ingredients to craft a menu that is both diverse and delectable.",
         img: restaurantReactImg,
         imgDesktop: restaurantReactImg,
-        technologies: ["React"],
+        technologies: [
+            {
+                title: "HTML",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "JavaScript",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "CSS",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+        ],
         visit: "https://stirring-phoenix-05f76a.netlify.app/",
         id: 13,
     },
@@ -52,7 +116,23 @@ export const projectsData = [
             "Elevate your fashion game with our exquisite collection of watches, crafted with precision and designed to make a bold statement – shop now to find your next favorite accessory.",
         img: watchesImg,
         imgDesktop: watchesImg,
-        technologies: ["HTML", "JavaScript", "CSS"],
+        technologies: [
+            {
+                title: "HTML",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "JavaScript",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "CSS",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+        ],
         visit: "https://a-s-t-r-a.github.io/responsive-watches-website/",
         id: 14,
     },
@@ -62,283 +142,24 @@ export const projectsData = [
             "Stay informed and inspired with our engaging and informative blog, featuring a variety of topics and perspectives that cater to your interests – explore now to expand your knowledge and curiosity.",
         img: blogImg,
         imgDesktop: blogImg,
-        technologies: ["HTML", "JavaScript", "CSS"],
+        technologies: [
+            {
+                title: "HTML",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "JavaScript",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+            {
+                title: "CSS",
+                tooltip:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
+            },
+        ],
         visit: "https://astra-blog.vercel.app/",
         id: 18,
     },
 ]
-
-/* import {
-    projImg1_1,
-    projImg1_2,
-    projImg1_3,
-    projImg2_1,
-    projImg2_2,
-    projImg2_3,
-    projImg3_1,
-    projImg3_2,
-    projImg3_3,
-    projImg4_1,
-    projImg4_2,
-    projImg4_3,
-    projImg5_1,
-    projImg5_2,
-    projImg5_3,
-    projImg9_1,
-    projImg10_1,
-} from "assets/images/projects" */
-
-/* export const NProjectsData = [
-    {
-        title: "Travel website",
-        description:
-            "This travel website offers a vast selection of destinations, accommodations, and activities, making it easy for travelers to plan and book their dream vacations.",
-        img: projImg9_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://a-s-t-r-a.github.io/responsive-travel-website/",
-        id: 8,
-    },
-    {
-        title: "Restaurant",
-        description:
-            "This restaurant website showcases their menu, location, and hours of operation, providing an easy and convenient way for customers to plan their visit and satisfy their cravings.",
-        img: projImg10_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://a-s-t-r-a.github.io/small-restaurant-js/",
-        id: 9,
-    },
-    {
-        title: "Sushi restaurant",
-        description:
-            "Welcome to our sushi restaurant website, where you can explore our menu, make a reservation, and order your favorite sushi dishes online for pickup or delivery.",
-        img: projImg4_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://cosmic-lokum-10d23a.netlify.app/",
-        id: 3,
-    },
-    {
-        title: "Beats store",
-        description:
-            "Experience the ultimate in sound quality with our headphones store website, where you can browse a wide selection of headphones and audio accessories, read reviews, and make a purchase, all from the comfort of your own home.",
-        img: projImg1_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://tranquil-pothos-611864.netlify.app/",
-        id: 0,
-    },
-    {
-        title: "Delivery website",
-        description:
-            "Our idelivery website provides a convenient way to get your desired products delivered right to your doorstep, whether it be groceries, electronics, or household items, saving you time and effort from going out to the store.",
-        img: projImg2_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://resplendent-marzipan-98ad86.netlify.app/",
-        id: 1,
-    },
-] */
-
-/* export const NProjectsData = [
-    {
-        title: "Travel website",
-        description:
-            "This travel website offers a vast selection of destinations, accommodations, and activities, making it easy for travelers to plan and book their dream vacations.",
-        img: projImg9_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://a-s-t-r-a.github.io/responsive-travel-website/",
-        id: 8,
-    },
-    {
-        title: "Restaurant",
-        description:
-            "This restaurant website showcases their menu, location, and hours of operation, providing an easy and convenient way for customers to plan their visit and satisfy their cravings.",
-        img: projImg10_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://a-s-t-r-a.github.io/small-restaurant-js/",
-        id: 9,
-    },
-    {
-        title: "Sushi restaurant",
-        description:
-            "Welcome to our sushi restaurant website, where you can explore our menu, make a reservation, and order your favorite sushi dishes online for pickup or delivery.",
-        img: projImg4_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://cosmic-lokum-10d23a.netlify.app/",
-        id: 3,
-    },
-    {
-        title: "Beats store",
-        description:
-            "Experience the ultimate in sound quality with our headphones store website, where you can browse a wide selection of headphones and audio accessories, read reviews, and make a purchase, all from the comfort of your own home.",
-        img: projImg1_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://tranquil-pothos-611864.netlify.app/",
-        id: 0,
-    },
-    {
-        title: "Delivery website",
-        description:
-            "Our idelivery website provides a convenient way to get your desired products delivered right to your doorstep, whether it be groceries, electronics, or household items, saving you time and effort from going out to the store.",
-        img: projImg2_1,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://resplendent-marzipan-98ad86.netlify.app/",
-        id: 1,
-    },
-] */
-
-/* export const projectsData = [
-    {
-        title: "Flight agency",
-        description:
-            "Start your journey with our flight agency website, where you can book flights, view travel deals, and get all the information you need to make your travel experience comfortable and hassle-free, including destination guides, luggage policies, and travel tips",
-        images: [projImg5_1, projImg5_2, projImg5_3],
-        tags: ["Web App"],
-        technologies: ["React", "SCSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        visit: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        id: 4,
-    },
-    {
-        title: "Sushi restaurant",
-        description:
-            "Welcome to our sushi restaurant website, where you can explore our menu, make a reservation, and order your favorite sushi dishes online for pickup or delivery.",
-        images: [projImg4_1, projImg4_2, projImg4_3],
-        tags: ["Landing Page"],
-        technologies: ["HTML", "JavaScript", "CSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://cosmic-lokum-10d23a.netlify.app/",
-        visit: "https://cosmic-lokum-10d23a.netlify.app/",
-        id: 3,
-    },
-
-    {
-        title: "Delivery website",
-        description:
-            "Our idelivery website provides a convenient way to get your desired products delivered right to your doorstep, whether it be groceries, electronics, or household items, saving you time and effort from going out to the store.",
-        images: [projImg2_1, projImg2_2, projImg2_3],
-        tags: ["Web App"],
-        technologies: ["HTML", "JavaScript", "CSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://resplendent-marzipan-98ad86.netlify.app/",
-        visit: "https://resplendent-marzipan-98ad86.netlify.app/",
-        id: 1,
-    },
-    {
-        title: "Halloween event",
-        description:
-            "Step into the spooky season with our Halloween event website, where you can find information on upcoming events and order items to get a taste of the Halloween fun you can expect.",
-        images: [projImg3_1, projImg3_2, projImg3_3],
-        tags: ["Landing Page"],
-        technologies: ["HTML", "JavaScript", "CSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://jade-pony-f69691.netlify.app/",
-        visit: "https://jade-pony-f69691.netlify.app/",
-        id: 2,
-    },
-
-    {
-        title: "Beats store",
-        description:
-            "Experience the ultimate in sound quality with our headphones store website, where you can browse a wide selection of headphones and audio accessories, read reviews, and make a purchase, all from the comfort of your own home.",
-        images: [projImg1_1, projImg1_2, projImg1_3],
-        tags: ["Web App"],
-        technologies: ["HTML", "JavaScript", "CSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://tranquil-pothos-611864.netlify.app/",
-        visit: "https://tranquil-pothos-611864.netlify.app/",
-        id: 0,
-    },
-] 
-
-export const projectsData = [
-    {
-        title: "Flight",
-        description:
-            "Start your journey with our flight agency website, where you can book flights, view travel deals, and get all the information you need to make your travel experience comfortable and hassle-free, including destination guides, luggage policies, and travel tips",
-        img: projImg3_1,
-        tags: ["Web App"],
-        technologies: ["React", "SCSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        visit: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        id: 4,
-    },
-    {
-        title: "Flight",
-        description:
-            "Start your journey with our flight agency website, where you can book flights, view travel deals, and get all the information you need to make your travel experience comfortable and hassle-free, including destination guides, luggage policies, and travel tips",
-        img: projImg5_1,
-        tags: ["Web App"],
-        technologies: ["React", "SCSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        visit: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        id: 2,
-    },
-    {
-        title: "Flight",
-        description:
-            "Start your journey with our flight agency website, where you can book flights, view travel deals, and get all the information you need to make your travel experience comfortable and hassle-free, including destination guides, luggage policies, and travel tips",
-        img: projImg5_1,
-        tags: ["Web App"],
-        technologies: ["React", "SCSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        visit: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        id: 1,
-    },
-    {
-        title: "Flight",
-        description:
-            "Start your journey with our flight agency website, where you can book flights, view travel deals, and get all the information you need to make your travel experience comfortable and hassle-free, including destination guides, luggage policies, and travel tips",
-        img: projImg5_1,
-        tags: ["Web App"],
-        technologies: ["React", "SCSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        visit: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        id: 7,
-    },
-    {
-        title: "Flight",
-        description:
-            "Start your journey with our flight agency website, where you can book flights, view travel deals, and get all the information you need to make your travel experience comfortable and hassle-free, including destination guides, luggage policies, and travel tips",
-        img: projImg5_1,
-        tags: ["Web App"],
-        technologies: ["React", "SCSS"],
-        dates: {
-            start: "14.05.2210",
-            finish: "14.05.2210",
-        },
-        source: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        visit: "https://sparkling-puffpuff-01ec61.netlify.app/",
-        id: 8,
-    },
-]
- */
