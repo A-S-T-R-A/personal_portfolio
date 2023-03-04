c6a6414b48cef1f290c1955eeb5efe389e4da741
je-delray <hovax74935@ekcsoft.com>
2023-03-04 09:42:17 +0100

    deleted comments

--- src/modules/NProjects/data.js
+++ src/modules/NProjects/data.js
@@ -1,17 +1,9 @@
-import beats from "assets/images/nProjects/beats.jpg"
-import delivery from "assets/images/nProjects/delivery.jpg"
-import halloween from "assets/images/nProjects/halloween.jpg"
-import sushi from "assets/images/nProjects/sushi.jpg"
 import plane from "assets/images/nProjects/plane.png"
 import restaurant_react from "assets/images/nProjects/restaurant_react.png"
 import watches from "assets/images/nProjects/watches.png"
-import christmas from "assets/images/nProjects/christmas.png"
-import travel from "assets/images/nProjects/travel.png"
-import coffee from "assets/images/nProjects/coffee.png"
 import blog from "assets/images/nProjects/blog.png"
 import realtor from "assets/images/nProjects/realtor.png"
 import beats2 from "assets/images/nProjects/beats2.png"
-import restaurant from "assets/images/nProjects/restaurant.png"
 
 export const projectsData = [
     {
@@ -24,16 +16,6 @@ export const projectsData = [
         visit: "https://real-estate-sable-six.vercel.app/",
         id: 8,
     },
-    /*     {
-        title: "Restaurant",
-        description:
-            "Join us at our restaurant and indulge in an unforgettable dining experience with exquisite dishes, hand-crafted cocktails, and exceptional service.",
-        img: restaurant,
-        imgDesktop: realtor,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://a-s-t-r-a.github.io/small-restaurant-js/",
-        id: 9,
-    }, */
     {
         title: "Headphones",
         description:
@@ -44,46 +26,6 @@ export const projectsData = [
         visit: "https://ecommerce-art1us.vercel.app/",
         id: 3,
     },
-    /*     {
-        title: "Beats store",
-        description:
-            "Experience studio-quality sound and ultimate comfort with our cutting-edge headphones, designed with the latest in audio technology and premium materials.",
-        img: beats,
-        imgDesktop: beats,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://tranquil-pothos-611864.netlify.app/",
-        id: 0,
-    }, */
-    /*     {
-        title: "Delivery website",
-        description:
-            "Our idelivery website provides a convenient way to get your desired products delivered right to your doorstep, whether it be groceries, electronics, or household items, saving you time and effort from going out to the store.",
-        img: delivery,
-        imgDesktop: delivery,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://resplendent-marzipan-98ad86.netlify.app/",
-        id: 1,
-    }, */
-    /*     {
-        title: "Halloween event",
-        description:
-            "Get ready for Halloween with our spooktacular store, featuring all the costumes, decorations, and accessories you need to create the ultimate Halloween experience!",
-        img: halloween,
-        imgDesktop: halloween,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://resplendent-marzipan-98ad86.netlify.app/",
-        id: 10,
-    }, */
-    /*     {
-        title: "Sushi restaurant",
-        description:
-            "Discover the freshest and most delicious sushi in town at our authentic Japanese restaurant.",
-        img: sushi,
-        imgDesktop: sushi,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://resplendent-marzipan-98ad86.netlify.app/",
-        id: 11,
-    }, */
     {
         title: "Flight Booking App",
         description:
@@ -114,38 +56,6 @@ export const projectsData = [
         visit: "https://a-s-t-r-a.github.io/responsive-watches-website/",
         id: 14,
     },
-    /*     {
-        title: "Christmas",
-        description:
-            "Embrace the magic of the holiday season and create unforgettable memories with our wide selection of festive decorations, gifts, and accessories – shop now to make your Christmas dreams come true.",
-        img: christmas,
-        imgDesktop: christmas,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://resplendent-marzipan-98ad86.netlify.app/",
-        id: 15,
-    }, */
-
-    /*     {
-        title: "Travel website",
-        description:
-            "Explore the world and discover new adventures with our comprehensive selection of travel packages, flights, and accommodations – book now to start your journey.",
-        img: travel,
-        imgDesktop: travel,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://resplendent-marzipan-98ad86.netlify.app/",
-        id: 16,
-    }, */
-    /*     {
-        title: "Coffee store",
-        description:
-            "Experience the rich, bold flavors of our premium coffee blends, expertly crafted to elevate your morning routine – shop our collection now to savor the perfect cup every day.",
-        img: coffee,
-        imgDesktop: coffee,
-        technologies: ["HTML", "JavaScript", "CSS"],
-        visit: "https://resplendent-marzipan-98ad86.netlify.app/",
-        id: 17,
-    }, */
-
     {
         title: "Blog website",
         description:
