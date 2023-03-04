eb2e0c86af28e0f44ce59acc4d353988735a983e
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-04 11:55:03 +0200

    added about icons

---
+++ src/assets/images/about/index.js
@@ -0,0 +1,6 @@
+import reactNativeIcon from "./mobile-development.png"
+import webDesignIcon from "./web-design.png"
+import frontendIcon from "./data.png"
+import backendIcon from "./development.png"
+
+export { reactNativeIcon, webDesignIcon, frontendIcon, backendIcon }
